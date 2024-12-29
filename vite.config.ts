dd8aaaa7bb20b834bd9c7815dac92bda685bcc3c
lixiaocong <xiaocong.lee@icloud.com>
2024-12-29 12:55:43 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
 import mockDevServerPlugin from 'vite-plugin-mock-dev-server'
+import postcssPxToViewport from 'postcss-px-to-viewport'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,10 +17,23 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
     mockDevServerPlugin(),
+    AutoImport({
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      resolvers: [VantResolver()],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [postcssPxToViewport({
+        viewportWidth: 375,
+      })],
     },
   },
   server: {
